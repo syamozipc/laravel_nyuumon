3a3c3dd1ab06085e0b8c550e8f5a436e92c911d0
syamozipc <syamozipc@gmail.com>
2021-03-05 22:28:43 +0900

    p64から続き

---
+++ resources/views/hello/index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>Hello/Index</title>
+    <style>
+        body {
+            font-size: 16pt; color: #999;
+        }
+        h1 {
+            font-size: 50pt;
+            /* text-align: right; */
+            color: #f6f6f6;
+        }
+    </style>
+</head>
+<body>
+    <h1>Index</h1>
+    <p><?= $msg; ?></p>
+    <p><?= date("Y年n月j日"); ?></p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +16,34 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('hello', function () {
-    return '<html><body><h1>Hello</h1><p>This is sample page</p></body></html>';
+Route::get('hi', function () {
+    return '<html><body><h1>Hi!!</h1><p>This is sample page</p></body></html>';
 });
+
+Route::get('hola/{msg}/{comment?}', function ($msg, $comment="default") {
+
+    $html = <<<EOF
+        <html>
+        <head>
+        <title>hola</title>
+        <style>
+        body {font-size:16pt; color:#999;}
+        h1 {font-size:100pt; text-align:right; color:#eee; margin:-40px 0px -50px 0px}
+        </style>
+        </head>
+        <body>
+            <h1>hola</h1>
+            <p>{$msg}</p>
+            <p>サンプルページ{$comment}</p>
+        </body>
+        </html>
+        EOF;
+
+    return $html;
+});
+
+// Route::get('hello', function () {
+//     return view('hello.index');
+// });
+
+Route::get('hello', 'HelloController@index');
